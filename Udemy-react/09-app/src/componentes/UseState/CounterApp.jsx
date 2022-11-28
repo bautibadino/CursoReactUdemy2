2974a5c1d672f157a70b3b900194a90ec53baade
Bautista <bautistabadino@gmail.com>
2022-11-28 10:30:13 -0300

    ep117

--- Udemy-react/09-app/src/componentes/UseState/CounterApp.jsx
+++ Udemy-react/09-app/src/componentes/UseState/CounterApp.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react"
 
 export const CounterApp = () => {
-    const [counter, setCounter] = useState(0)
-    
+    const [{counter1 , counter2, counter3}, setCounter] = useState({
+      counter1 : 10,
+      counter2 : 20,
+      counter3 : 30,
+    })
     return (
         <>
         
-        <h1>Counter: {counter}</h1>
+        <h1>Counter: {counter1}</h1>
     
-        <button onClick={ () => setCounter(counter + 1)}>+1</button>
-        <button onClick={ () => setCounter(counter - 1)}>-1</button>
+        <button onClick={ () => setCounter({counter1: counter1 + 1})}>+1</button>
+        <button onClick={ () => setCounter({counter1: counter1 - 1})}>-1</button>
 
         
         </>
